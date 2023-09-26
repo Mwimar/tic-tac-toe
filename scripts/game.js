d471a25cd98da9a78f86ac7f384ea3ded34a289f
Mark Mwima <markmwimar@gmail.com>
2023-09-26 21:28:05 +0300

    switching between active players

--- scripts/game.js
+++ scripts/game.js
@@ -6,9 +6,16 @@ function startNewGame() {
     gameArea.style.display = 'block';
 }
 
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
 function selectGameField(event) {
     event.target.textContent = players[activePlayer].symbol;
     event.target.classList.add('disabled');
-
-
+    switchPlayer();
 }
